c76244480125855545b1f15f2da58311654d4d2c
izakdvlpr <izakdvlpr@gmail.com>
2026-02-10 22:59:01 -0300

    feat: create review, progress, read, favorite and litst tables

--- prisma/migrations/20260209154714_batata/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `video` on the `movies` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "movies" DROP COLUMN "video",
-ADD COLUMN     "videos" JSONB;
--- prisma/migrations/20260209155418_socorro/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `releaseDate` column on the `movies` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "movies" DROP COLUMN "releaseDate",
-ADD COLUMN     "releaseDate" TIMESTAMP(3);
