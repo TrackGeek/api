b6b2efffc7a348d16d05d2f564c69536948e1da1
izakdvlpr <izakdvlpr@gmail.com>
2026-02-12 18:10:06 -0300

    feat(email): refactor sendMagicLink to use HTML template for email content

--- src/shared/infra/email/email.service.ts
+++ src/shared/infra/email/email.service.ts
@@ -1,6 +1,9 @@
+import fs from 'node:fs'
+import path from 'node:path'
 import { Injectable } from "@nestjs/common";
 import { ResendService } from "nestjs-resend";
 import { ConfigService } from "@nestjs/config";
+import handlebars from 'handlebars'
 
 import { SendMagicLinkDto } from "./dtos/send-magic-link.dto";
 import { extractNameFromEmail } from "@/shared/utils/email";
@@ -11,43 +14,26 @@ export class EmailService {
 		private readonly resendService: ResendService,
 		private readonly configService: ConfigService,
 	) {}
+  
+  private getHtmlTemplate(templateName: string, variables: Record<string, any>): string {
+    const templatePath = path.join(__dirname, 'templates', `${templateName}.template.hbs`);
+    const templateSource = fs.readFileSync(templatePath, 'utf-8');
+    const template = handlebars.compile(templateSource);
+    return template(variables);
+  }
 
 	async sendMagicLink(sendMagicLinkDto: SendMagicLinkDto) {
 		const { email, url } = sendMagicLinkDto;
+    
+    const name = extractNameFromEmail(email);
+    
+    const html = this.getHtmlTemplate('send-magic-link', { name, url });
 
 		await this.resendService.send({
 			from: this.configService.get<string>("RESEND_FROM")!,
 			to: email,
 			subject: "Sign in to TrackGeek",
-			html: `
-        <body style="margin: 0; padding: 40px 20px; font-family: Arial, sans-serif; background-color: #1c1917;">
-          <table role="presentation" style="max-width: 600px; margin: 0 auto; background-color: #1c1917; border: 1px solid #ffffff1a; border-radius: 8px; padding: 60px 40px;">
-            <tr>
-              <td style="text-align: center;">
-                <h2 style="margin: 0 0 20px 0; color: #ffffff; font-size: 24px; font-weight: 500; line-height: 1.3;">
-                  Hello, ${extractNameFromEmail(email)}!
-                </h2>
-                
-                <p style="margin: 0 0 30px 0; color: #a6a09b; font-size: 18px; line-height: 1.5;">Click the button below to securely sign in to your account:</p>
-              </td>
-            </tr>
-            <tr>
-              <td style="text-align: center; padding: 20px 0;">
-                <a href="${url}" style="display: inline-block; background-color: #10b981; color: #ffffff; font-size: 24px; font-weight: 600; padding: 20px; text-decoration: none; border-radius: 8px;">
-                  Sign in to TrackGeek
-                </a>
-              </td>
-            </tr>
-            <tr>
-              <td style="text-align: center; padding-top: 20px;">
-                <p style="margin: 0; color: #a6a09b; font-size: 16px; line-height: 1.5;">
-                  If you did not request this link, please ignore this email.
-                </p>
-              </td>
-            </tr>
-          </table>
-        </body>
-      `,
+			html,
 		});
 	}
 }
