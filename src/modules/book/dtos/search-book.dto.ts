b22912bed59cc44dfc75e3758bdb9ba719d1c724
izakdvlpr <izakdvlpr@gmail.com>
2026-02-25 13:03:21 -0300

    feat: add MinLength validation to search DTOs for query parameter

--- src/modules/book/dtos/search-book.dto.ts
+++ src/modules/book/dtos/search-book.dto.ts
@@ -1,6 +1,7 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, MinLength } from "class-validator";
 
 export class SearchBookDto {
 	@IsNotEmpty()
+	@MinLength(3)
 	readonly query: string;
 }
