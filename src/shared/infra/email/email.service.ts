0042bbac8c1dddf02d58e6bc7118da74fa41e2fd
izakdvlpr <izakdvlpr@gmail.com>
2026-03-02 20:45:39 -0300

    fix(email): email template dir

--- src/shared/infra/email/email.service.ts
+++ src/shared/infra/email/email.service.ts
@@ -6,7 +6,7 @@ import handlebars from "handlebars";
 import { ResendService } from "nestjs-resend";
 import { extractNameFromEmail } from "@/shared/utils/email";
 import { MagicLinkEmailDto } from "./dtos/magic-link-email.dto";
-import { ResetPasswordEmailDto } from './dtos/reset-password-email.dto';
+import { ResetPasswordEmailDto } from "./dtos/reset-password-email.dto";
 
 @Injectable()
 export class EmailService {
@@ -16,15 +16,14 @@ export class EmailService {
   ) {}
 
   private getHtmlTemplate(templateName: string, variables: Record<string, any>): string {
-    const baseDir = process.env.NODE_ENV === "development"
-      ? path.join(process.cwd(), "src", "shared", "infra", "email")
-      : __dirname;
+    const baseDir =
+      process.env.NODE_ENV === "development" ? path.join(process.cwd(), "src", "shared", "infra", "email") : __dirname;
 
     const templatePath = path.join(baseDir, "templates", `${templateName}.template.hbs`);
     const templateSource = fs.readFileSync(templatePath, "utf-8");
-    
+
     const template = handlebars.compile(templateSource);
-    
+
     return template(variables);
   }
 
@@ -40,7 +39,7 @@ export class EmailService {
       html: this.getHtmlTemplate("magic-link-email", { name, url }),
     });
   }
-  
+
   async sendResetPasswordEmail(resetPasswordEmailDto: ResetPasswordEmailDto) {
     const { name, email, url } = resetPasswordEmailDto;
 
@@ -49,6 +48,6 @@ export class EmailService {
       to: email,
       subject: "Reset your password for TrackGeek",
       html: this.getHtmlTemplate("reset-password-email", { name, url }),
-    })
+    });
   }
 }
