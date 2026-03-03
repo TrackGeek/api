1019ff669ce82d6c4d99dcb21eb63dba24931eb9
izakdvlpr <izakdvlpr@gmail.com>
2026-03-03 18:02:31 -0300

    fix: add leading slash to POST route in upload controller for consistency

--- src/shared/infra/upload/upload.controller.ts
+++ src/shared/infra/upload/upload.controller.ts
@@ -9,7 +9,7 @@ import { imageConfig } from './upload.config';
 export class UploadController {
   constructor(private readonly uploadService: UploadService) {}
 
-  @Post("image")
+  @Post("/image")
   @UseInterceptors(FileInterceptor("file", imageConfig))
   async uploadImage(@UploadedFile() file: Express.Multer.File) {
     const imageUrl = await this.uploadService.uploadFromBuffer(file.buffer);
