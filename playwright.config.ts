c935e4af85fb48cdee38dc3cc1d33167ddb1e3dc
izakdvlpr <izakdvlpr@gmail.com>
2026-03-03 10:08:18 -0300

    feat(playwright): update test directory path in configuration

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { defineConfig, devices } from "@playwright/test";
 
 export default defineConfig({
-  testDir: "./e2e",
+  testDir: "./test/e2e",
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
