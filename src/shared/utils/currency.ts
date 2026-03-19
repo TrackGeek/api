df03f0a86a4e194c559398550a30f0f7ba5f1b62
izakdvlpr <izakdvlpr@gmail.com>
2026-03-18 23:20:08 -0300

    fix(currency): correct currency formatting and restore user currency retrieval logic

--- src/shared/utils/currency.ts
+++ src/shared/utils/currency.ts
@@ -3,18 +3,16 @@ import type { ClientIpType } from "../decorators/client-ip.decorator";
 import { DEFAULT_CURRENCY } from "../constants/payment";
 
 export function formatValue(value: number, currency: string) {
-  return new Intl.NumberFormat("en-US", { style: "currency", currency }).format(value / 100);
+  return new Intl.NumberFormat("en-US", { style: "currency", currency }).format(value);
 }
 
 export async function getUserCurrency(clientIp?: ClientIpType) {
-  // if (!clientIp || !clientIp?.isLocal) return DEFAULT_CURRENCY;
+  if (!clientIp || !clientIp?.isLocal) return DEFAULT_CURRENCY;
 
-  // return axios
-  //   .get(`https://ipapi.co/${clientIp.address}/json/`)
-  //   .then((response) =>
-  //     response.data ? String(response.data?.currency ?? DEFAULT_CURRENCY).toLowerCase() : DEFAULT_CURRENCY,
-  //   )
-  //   .catch(() => DEFAULT_CURRENCY);
-  
-  return 'brl'
+  return axios
+    .get(`https://ipapi.co/${clientIp.address}/json/`)
+    .then((response) =>
+      response.data ? String(response.data?.currency ?? DEFAULT_CURRENCY).toLowerCase() : DEFAULT_CURRENCY,
+    )
+    .catch(() => DEFAULT_CURRENCY);
 }
