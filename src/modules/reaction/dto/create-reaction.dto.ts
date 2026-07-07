078c6be4dd7962f6232956f6629b7d1788b58970
izakdvlpr <izakdvlpr@gmail.com>
2026-07-07 18:54:48 -0300

    feat(reaction): add optional fields for various review types in CreateReactionDto and GetReactionsDto

--- src/modules/reaction/dto/create-reaction.dto.ts
+++ src/modules/reaction/dto/create-reaction.dto.ts
@@ -59,4 +59,34 @@ export class CreateReactionDto {
     description: "Required when type is FeedEvent",
   })
   readonly feedEventId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.GameReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is GameReview" })
+  readonly gameReviewId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.AnimeReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is AnimeReview" })
+  readonly animeReviewId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.MangaReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is MangaReview" })
+  readonly mangaReviewId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.TvShowReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is TvShowReview" })
+  readonly tvShowReviewId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.MovieReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is MovieReview" })
+  readonly movieReviewId?: string;
+
+  @IsOptional()
+  @ReactionRequiredForType(ReactionType.BookReview)
+  @ApiProperty({ type: "string", format: "uuid", required: false, description: "Required when type is BookReview" })
+  readonly bookReviewId?: string;
 }
